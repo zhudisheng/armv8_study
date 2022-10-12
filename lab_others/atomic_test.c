885cab2e3acfe257269baa047aa4d3a647734e12
zhuxinran <zhuran@mail.ustc.edu.cn>
2022-10-12 13:28:54 +0800

    other instr lab03: ldxr与stxr指令的使用1
    Signed-off-by: zhuxinran <zhuran@mail.ustc.edu.cn>

---
+++ lab_others/atomic_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+extern void atomic_set(unsigned long a,unsigned long b);
+extern unsigned long my_atomic_write(long a);
+
+int main()
+{
+  unsigned long p1 = 0;
+  printf("p1 address: 0x%lx\n",&p1);
+
+  p1 = my_atomic_write(0x34);
+
+  printf("after atomic write:0x%x\n",p1);
+
+}
