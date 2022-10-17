f385d35cfd048a2db1af3bc7f22b85bed9e1adeb
zhuxinran <zhuran@mail.ustc.edu.cn>
2022-10-17 10:00:02 +0800

    连接器lab02:打印每个段的布局
    Signed-off-by: zhuxinran <zhuran@mail.ustc.edu.cn>

--- lab01/src/kernel.c
+++ lab01/src/kernel.c
@@ -71,6 +71,32 @@ void my_ldr_str_test(void)
 }
 extern unsigned long macro_test_1(long a,long b);
 extern unsigned long macro_test_2(long a,long b);
+
+extern char _text_boot[], _etext_boot[];
+extern char _text[], _etext[];
+extern char _rodata[], _erodata[];
+extern char _data[], _edata[];
+extern char _bss[], _ebss[];
+
+static void print_mem(void)
+{
+  printk("BenOS image layout:\n");
+  printk(".text.boot:0x%08lx - 0x%08lx (%6ld B)\n",
+      (unsigned long)_text_boot, (unsigned long)_etext_boot,
+      (unsigned long)(_etext_boot - _text_boot));
+  printk(".text:0x%08lx - 0x%08lx (%6ld B)\n",
+      (unsigned long)_text, (unsigned long)_etext,
+      (unsigned long)(_etext - _text));
+  printk(".rodata:0x%08lx - 0x%08lx (%6ld B)\n",
+      (unsigned long)_rodata, (unsigned long)_erodata,
+      (unsigned long)(_erodata - _rodata));
+  printk(".data:0x%08lx - 0x%08lx (%6ld B)\n",
+      (unsigned long)_data, (unsigned long)_edata,
+      (unsigned long)(_edata - _data));
+  printk(".bss:0x%08lx - 0x%08lx (%6ld B)\n",
+      (unsigned long)_bss, (unsigned long)_ebss,
+      (unsigned long)(_ebss - _bss));
+}
 void kernel_main(void)
 {
   unsigned long val = 0;
@@ -86,6 +112,7 @@ void kernel_main(void)
   print_func_name(0x800880);
   val = macro_test_1(5,5);
   val = macro_test_2(5,5);
+  print_mem();
   while(1) {
     uart_send(uart_recv());
   }
